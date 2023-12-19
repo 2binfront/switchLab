bc6bf785e0485a7d5b5f233dd9ce209a1a2144a1
2binfront <824225714@qq.com>
2023-12-19 18:03:28 +0800

    finishing lab4

--- lab-hh/lab_4/myrouter.py
+++ lab-hh/lab_4/myrouter.py
@@ -6,10 +6,13 @@ Basic IPv4 router (static routing) in Python.
 
 import time
 import switchyard
+import ipaddress
 from switchyard.lib.userlib import *
 
 class TableRow:
     def __init__(self,prefix,mask,nextHop,portName):
+        self.subnet=IPv4Network(f'{prefix}/{mask}')
+
         self.prefix=IPv4Address(prefix)
         self.mask=IPv4Address(mask)
         self.nextHop=IPv4Address(nextHop)        
@@ -19,14 +22,22 @@ class TableRow:
 # 核心是想明白如何把来数据包的ip与表项逐项比对寻找出最长前缀匹配，
 # python内置有 IPv4Network IPv4Address对象
 # 前者用于整合ip地址和mask，快速得出mask位数；后者用于与int()结合快速掩码ip，并进行ip比对
-# 那么比对流程就是循环每个 TableRow,逐个寻找符合最长前缀匹配的表项，若符合再比较掩码位数，
+# 也可以直接用前者来标识子网
+# 那么比对流程就是 摘取出IP包中的目的IP
+# 循环每个 TableRow,逐个寻找符合最长前缀匹配的表项，若符合再比较掩码位数，
 # 记住并更新最长前缀匹配表项index。不存在则为初始值-1
+        
+
+# 匹配到对应项后，IP header中TTL值-1，暂时不管过期
+# 生成一个新的ethernet header，要根据next hop值来设置目标mac值。
+# next hop的值不存在时需要生成arp query向其他人询问
 
 class Router(object):
     def __init__(self, net: switchyard.llnetbase.LLNetBase):
         self.net = net
         # other initialization stuff here
         self.portsList=net.interfaces()
+        self.generateTable()
 
     def generateTable(self):
         table=[]
@@ -37,21 +48,24 @@ class Router(object):
                 table.append(TableRow(rowList[0],rowList[1],rowList[2],rowList[3]))
                 line = fd.readline()
         for port in self.net.portsList:
-            table.append(TableRow(port.ipaddr,))
+            table.append(TableRow(port.ipaddr,port.netmask,None,port.name))
 
+        self.table=table
 
-        
-
-
-    def handle_packet(self, recv: switchyard.llnetbase.ReceivedPacket):
+    def handle_packet(self, recv: switchyard.llnetbase.ReceivedPacket):        
         timestamp, ifName, packet = recv
         # TODO: your logic here
         arpTable={}
         arp = packet.get_header(Arp)
+
+        EthernetHeader=packet.get_header(Ethernet)
+        IPv4Header=packet.get_header(IPv4)
+
+        # search arp table if is arp query packet
         if arp:
             arpTable[arp.senderprotoaddr]=arp.senderhwaddr
-            for key,val in arpTable.items():
-                print(key,' arp pair ',val)
+            # for key,val in arpTable.items():
+            #     print(key,' arp pair ',val)
             for port in self.portsList:
                 if arp.targetprotoaddr == port.ipaddr:
                     rePacket = Ethernet()
@@ -70,6 +84,26 @@ class Router(object):
                         #original arp request ipaddr
                         targetprotoaddr= arp.senderprotoaddr)
                     self.net.send_packet(ifName,rePacket)
+
+        # search forwarding table if not arp query
+        elif IPv4Header:
+            tarIndex=-1
+            maxMask=0
+            for index,row in enumerate(self.table):
+                if IPv4Address(IPv4Header.src) in row.subnet and row.subnet.prefixlen>maxMask:
+                    maxMask=row.subnet.prefixlen 
+                    tarIndex=index
+                    log_info(f'found target {index}')
+                continue
+            if tarIndex!=-1:
+                IPv4Header.ttl-=1
+                if (self.table[tarIndex].nextHop) in arpTable.keys():
+
+                    
+                
+        else:
+            log_failure('no IP header found')
+
 
 
     def start(self):
